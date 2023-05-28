ec569f578adf1491df0481298bbc7f2c5e5990a5
ihor-vip <vip20140902@gmail.com>
2023-05-28 11:30:03 +0300

    create jsx for SongCard.jsx, change the address of API(fix)

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const shazamCoreApi = createApi({
     reducerPath: 'shazamCoreApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://shazam-core7.p.rapidapi.com/',
+        baseUrl: 'https://shazam-core.p.rapidapi.com/',
         prepareHeaders: (headers) => {
             headers.set('X-RapidAPI-Key', import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY);
 
@@ -11,7 +11,7 @@ export const shazamCoreApi = createApi({
         },
     }),
     endpoints: (builder) => ({
-        getTopCharts: builder.query({ query: () => 'charts/get-top-songs-in-world' }),
+        getTopCharts: builder.query({ query: () => 'v1/charts/world' }),
     }),
 });
 
