eee8630492c5df30033da1cce0d6a59ac40a3ed9
ihor-vip <vip20140902@gmail.com>
2023-05-28 11:49:59 +0300

    add playing functionality to SongCard.jsx, complete Discover page and make music player operational

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { Error, Loader, SongCard } from '../components';
 import { genres } from '../assets/constants';
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 const Discover = () => {
+    const dispatch = useDispatch();
+    const { activeSong, isPlaying } = useSelector((state) => state.player);
     const { data, isFetching, error } = useGetTopChartsQuery();
     const genreTitle = 'Pop';
 
@@ -31,6 +34,9 @@ const Discover = () => {
                     <SongCard
                         key={song.key}
                         song={song}
+                        isPlaying={isPlaying}
+                        activeSong={activeSong}
+                        data={data}
                         i={i}
                     />
                 ))}
